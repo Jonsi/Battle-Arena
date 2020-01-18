a58893ac6274aaf82b9da158184addc352d10bd0
JonsiBonsi <jons1122@gmail.com>
2020-01-18 13:59:37 +0200

    Initial Commit

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform PlayerTransform;
+
+    private Vector3 _cameraOffset;
+
+    [Range(0.01f, 1f)]
+    public float CameraSmooth = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cameraOffset = transform.position - PlayerTransform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        var newPos = PlayerTransform.position + _cameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, CameraSmooth);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody Rigidbody;
+    public FixedJoystick Joystick;
+
+    public float Speed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            var velocity = new Vector3(Joystick.Horizontal * Speed, Rigidbody.velocity.y, Joystick.Vertical * Speed);
+            Rigidbody.velocity = velocity;
+
+            transform.rotation = Quaternion.LookRotation(Rigidbody.velocity);
+        }
+
+    }
+}
