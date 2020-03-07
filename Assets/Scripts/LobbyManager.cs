caf60dea829c5983c7c5160b1ca8102104210bfe
JonsiBonsi <jons1122@gmail.com>
2020-03-07 10:42:39 +0200

    MES-7 - LobbyManager fix

--- Assets/Scripts/LobbyManager.cs
+++ Assets/Scripts/LobbyManager.cs
@@ -1,18 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class LobbyManager : MonoBehaviour
+public class LobbyManager : MonoBehaviourPunCallbacks
 {
+    public static LobbyManager Singleton;
+
+    public Button JoinGameButton;
+
+    public byte maxRoomPlayers = 4;
+    public int maxRoomsNumber = 4;
+
+    private void Awake()
+    {
+        Singleton = this;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PhotonNetwork.ConnectUsingSettings();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        Debug.Log("Connected To Master");
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    public void OnJoinButtonClicked()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Failed Joining Room");
+        CreateRoom();
+    }
+
+    public void CreateRoom()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.IsOpen = true;
+        roomOptions.IsVisible = true;
+        roomOptions.MaxPlayers = maxRoomPlayers;
+
+        var roomName = "Room" + Random.Range(0f, maxRoomsNumber);
+        PhotonNetwork.CreateRoom(roomName,roomOptions);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("Room Created");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined room");
     }
 }
